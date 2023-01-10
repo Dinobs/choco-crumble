244d2760d465a4fd5438a1451dd5d619944f81e6
Mathilde Guédon <maguedon@laposte.net>
2023-01-10 10:53:18 +0100

    design category list

--- app/src/main/java/com/example/chococrumble/ui/CategoriesAdapter.kt
+++ app/src/main/java/com/example/chococrumble/ui/CategoriesAdapter.kt
@@ -3,13 +3,17 @@ package com.example.chococrumble.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chococrumble.R
 import com.example.chococrumble.model.Category
+import com.squareup.picasso.Picasso
 
 class CategoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var textView: TextView = itemView.findViewById(R.id.category_item_textview)
+    var descriptionTextView: TextView = itemView.findViewById(R.id.category_description_item_textview)
+    var imageView: ImageView = itemView.findViewById(R.id.category_imageview)
 }
 
 class CategoriesAdapter(private val categories: List<Category>, val onClick: (String) -> Unit): RecyclerView.Adapter<CategoriesViewHolder>() {
@@ -21,8 +25,10 @@ class CategoriesAdapter(private val categories: List<Category>, val onClick: (St
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, index: Int) {
         holder.textView.text = categories[index].name
+        holder.descriptionTextView.text = categories[index].description
+        Picasso.get().load(categories[index].thumb).into(holder.imageView)
 
-        holder.textView.setOnClickListener {
+        holder.itemView.setOnClickListener {
             categories[index].name?.let { it1 -> onClick(it1) }
         }
     }
